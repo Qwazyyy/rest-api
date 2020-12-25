78fbaab183a624fe0c2d35766accd5f0181ef1b0
Qwazyyy <52658928+Qwazyyy@users.noreply.github.com>
2020-12-26 01:53:35 +0500

    Update films.php

--- api/films.php
+++ api/films.php
@@ -1,21 +1,4 @@
 <?php
-    //вывод массива в формате JSON
-    // function getArrayFromJSON($query)
-    // {
-    //     $input_array = [];
-    //     while($object = $query->fetch(PDO::FETCH_ASSOC))
-    //     {
-    //         $input_array[] = $object;
-    //     }
-    //     return json_encode($input_array, JSON_UNESCAPED_UNICODE);
-    // }
-
-    // //вывод строки в формате JSON
-    // function getObjectFromJSON($query)
-    // {
-    //     return json_encode($query->fetch(PDO::FETCH_ASSOC), JSON_UNESCAPED_UNICODE);
-    // }
-
     //вывод всех фильмов
     function getAllFilms($connection)
     {
