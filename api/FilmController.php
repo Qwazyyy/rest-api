6bf5147d76487de0f327c019716158ebc7cc07c7
Qwazyyy <07john00@mail.ru>
2021-01-10 16:06:07 +0500

    Добавлен контроллер для фильмов
    Добавлен контроллер для фильмов(пока не полный функционал)

---
+++ api/FilmController.php
@@ -0,0 +1,48 @@
+<?php
+    interface FilmControllerInterface
+    {
+        public static function callGetMethodService($parameters);
+        public static function callPostMethodService($parameters);
+        public static function callPutMethodService($parameters);
+        public static function callDeleteMethodService($parameters);
+    }
+
+    class FilmController implements FilmControllerInterface
+    {
+        public static function callGetMethodService($parameters)
+        {
+            if(count($parameters) == 1) {
+                //die(print_r($parameters));
+                $answer = FilmService::getAll();
+                return $answer;
+            }
+            if(count($parameters) == 2) {
+                $answer = FilmService::getById($parameters[1]);
+                return $answer;
+            }
+            if(count($parameters) == 5) {
+                $answer = FilmService::getByActorGenreAndFilter($parameters);
+                return $answer;
+            }
+        }
+        public static function callPostMethodService($parameters)
+        {
+            // if() {
+
+            // }
+        }
+        public static function callPutMethodService($parameters)
+        {
+            // if() {
+
+            // }
+        }
+        public static function callDeleteMethodService($parameters)
+        {
+            $answer = FilmService::delete($parameters[1]);
+            return $answer;
+            // if() {
+
+            // }
+        }
+    }
