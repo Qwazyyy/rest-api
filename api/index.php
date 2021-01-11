b32b5d52a567925a77d35dea9705771e4bf89788
Qwazyyy <07john00@mail.ru>
2021-01-11 12:37:26 +0500

    новый index.php

--- api/index.php
+++ api/index.php
@@ -1,144 +1,30 @@
 <?php
     header('Content-type: application/json');
+    
+    ini_set('error_reporting', E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
 
-    include_once('jsoninput.php');
-    include_once('actors.php');
-    include_once('genres.php');
-    include_once('films.php');
-    include_once('connection.php');
+    include_once('Services/jsoninput.php');
 
+    include_once('Services/FilmService.php');
+    include_once('Services/ActorService.php');
+    include_once('Services/GenreService.php');
 
-    $method = $_SERVER['REQUEST_METHOD'];
-    //die(print_r($_POST));
-    $q = $_GET['q'];
-    $params = explode('/', $q);
-    $type = $params[0];
-
-    if(count($params) == 2)
-    {
-        $id = $params[1];
-    }
-    elseif(count($params) == 5)
-    {
-        $genre = $params[1];
-        $actor_first_name = $params[2];
-        $actor_last_name = $params[3];
-        $order_by = $params[4];
-    }
-    else
-    {
-
-    }
-    //die(print_r($_POST));
-    switch ($method) {
-        case GET:
-            if($type === 'films')
-            {
-                //получение фильма по id
-                if(isset($id))
-                {
-                    echo json_encode('test');
-                    echo getFilmById($connection, $id);
-                } 
-                //получение фильма по имени актера и по жанру и фильтрация по жанру или актеру
-                elseif(isset($actor_first_name))
-                {
-                    echo getFilmByActorAndGenre($connection, $genre, $actor_first_name, $actor_last_name, $order_by);
-                }
-                //получение всех фильмов
-                else
-                {
-                    echo getAllFilms($connection);
-                }
-            }
-            else
-            {
-                echo json_encode('Bad request');
-            }
-            break;
+    include_once('Services/Connection.php');
+    include_once('Services/UrlService.php');
 
 
-        case POST:
+    include_once('Repositories/FilmRepository.php');
+    include_once('Repositories/ActorRepository.php');
+    include_once('Repositories/GenreRepository.php');
 
-            if($type === 'films')
-            {
-                $data = $_POST;
-                echo postFilm($connection, $data);
-            }
-            elseif($type === 'actors')
-            {
-                $data = $_POST;
-                echo postActor($connection, $data);
-            }
-            else
-            {
-                $data = $_POST;
-                echo postGenre($connection, $data);
-            }
-            break;
+    include_once('Controllers/ActorController.php');
+    include_once('Controllers/FilmController.php');
+    include_once('Controllers/GenreController.php');
+    include_once('Controllers/Controller.php');
+    
+    $requestMethod = $_SERVER['REQUEST_METHOD'];
 
-
-        case PUT:
-
-            if($type === 'films')
-            {
-                if(isset($id))
-                {
-                    $data = file_get_contents('php://input');
-                    //die(print_r($data));
-                    $data = json_decode($data, true);
-                    // die(print_r($data));
-                    // die(print_r($data['name']));
-                    echo putFilm($connection, $id, $data);
-                }
-            }
-            elseif($type === 'actors')
-            {
-                if(isset($id))
-                {   
-                    $data = file_get_contents('php://input');
-                    //die(print_r($data));
-                    $data = json_decode($data, true);
-                    //die(print_r($data));
-                    echo putActor($connection, $id, $data); 
-                }
-            }
-            else
-            {
-                if(isset($id))
-                {   
-                    $data = file_get_contents('php://input');
-                    //die(print_r($data));
-                    $data = json_decode($data, true);
-                    //die(print_r($data));
-                    echo putGenre($connection, $id, $data); 
-                }
-            }
-            break;
-
-
-        case DELETE:
-            if($type === 'films')
-            {
-                if(isset($id))
-                {
-                    echo deleteFilm($connection, $id);
-                }
-            }
-            elseif($type === 'actors')
-            {
-                if(isset($id))
-                {
-                    echo deleteActor($connection, $id);
-                }
-            }
-            else
-            {
-                if(isset($id))
-                {
-                    echo deleteGenre($connection, $id);
-                }
-            }
-            break;
-    }
-?> 
+    $parameters = UriService::getUriParameters();
+    echo Controller::callEntityController($requestMethod, $parameters);
